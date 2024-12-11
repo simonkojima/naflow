86923eed8575fd68d7c314601956ae6fbc4445f1
Simon Kojima <simon.kojima@outlook.com>
2024-12-11 13:27:20 +0900

    update

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+#import pydata_sphinx_theme
 
 root_path = Path(__file__).parent.parent
 sys.path.insert(0, str(root_path.absolute()))
@@ -20,14 +21,37 @@ release = '0.0.3'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+#extensions = [
+#    "sphinx.ext.autodoc",
+#    "sphinx.ext.viewcode",
+#    "sphinx.ext.todo",
+#    "sphinx.ext.napoleon",
+#    #"sphinx_rtd_theme",#
+#    "pydata_sphinx_theme",
+#    "myst_parser",
+#]
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
     "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.imgmath",
     "sphinx.ext.napoleon",
-    #"sphinx_rtd_theme",
-    "pydata_sphinx_theme",
-    "myst_parser",
+    "sphinx.ext.linkcode",
+    "sphinx.ext.mathjax",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinx_gallery.gen_gallery",
+    "gh_substitutions",
+    "m2r2",
+    "numpydoc",
+    "sphinx_favicon",
+    "sphinxcontrib.jquery",
 ]
 
 templates_path = ['_templates']
@@ -40,7 +64,9 @@ python_use_unqualified_type_names = True
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+import sphinx_rtd_theme
 html_theme = 'pydata_sphinx_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
 source_suffix = {
